6f851d024153e64a43d802650fbdd70146d07819
proffbraun <proffbraun@gmail.com>
2014-09-07 23:36:57 +0600

    export to unimarc for next indexing

--- z3950-app/src/main/java/kz/arta/ext/z3950/convert/AUnimarcConverter.java
+++ z3950-app/src/main/java/kz/arta/ext/z3950/convert/AUnimarcConverter.java
@@ -16,11 +16,11 @@ public abstract class AUnimarcConverter extends AMarcConverter {
         Record record = factory.newRecord();
         ControlField cf = factory.newControlField(CONTROL_ID_TAG, fieldsWrapper.getUuid());
         record.addVariableField(cf);
-        for (FormField formField : fieldsWrapper.getFields()) {
+        for (FormField formField : fieldsWrapper.getFormFieldMap().values()) {
             String id = formField.getId();
             String val = formField.getValue();
             String first = id.substring(0, 1);
-            if (!StringUtils.isNumber(first) || id.length() > 4 || CONTROL_ID_TAG.equals(id)
+            if (!StringUtils.isNumber(first) || CONTROL_ID_TAG.equals(id)       //id.length() > 4 ||
                     || val == null || "".equals(val.trim())) {
                 continue;
             }
@@ -32,7 +32,17 @@ public abstract class AUnimarcConverter extends AMarcConverter {
             String fieldTag = id.substring(0, 3);
             DataField dataField = (DataField) record.getVariableField(fieldTag);
             if (dataField == null) {
-                dataField = factory.newDataField(fieldTag, ' ', ' ');
+                char ind1 = ' ';
+                if (fieldTag.equals("200")){
+                    ind1 = '1';
+                }
+                char ind2 = ' ';
+                if (fieldTag.equals("700")){
+                    ind2 = '1';
+                }
+
+                //todo !!! идентификаторы для полей
+                dataField = factory.newDataField(fieldTag, ind1, ind2);
 //                dataField.setTag(fieldTag);
                 record.addVariableField(dataField);
             }
