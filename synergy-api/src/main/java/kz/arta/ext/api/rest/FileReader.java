c9dce965436fd79a9516eee9ae22108b7d13ddd8
proffbraun <proffbraun@gmail.com>
2014-10-09 11:14:09 +0600

    fix error load files

--- synergy-api/src/main/java/kz/arta/ext/api/rest/FileReader.java
+++ synergy-api/src/main/java/kz/arta/ext/api/rest/FileReader.java
@@ -15,7 +15,7 @@ public class FileReader extends RestQuery {
     public String startUploadFile(RestQueryContext context) {
         try {
 
-            String query = "rest/api/storage/start_upload";
+            String query = "/rest/api/storage/start_upload";
             String resultData = doGetQuery(context, query);
             log.debug("resultData = {}", resultData);
             ObjectMapper objectMapper = new ObjectMapper();
@@ -33,7 +33,7 @@ public class FileReader extends RestQuery {
     public boolean uploadPart(RestQueryContext context, String file, String data) {
         try {
             log.debug("uploadPart size ={}", data.length());
-            String query = String.format("rest/api/storage/uploadPart?file=%s",
+            String query = String.format("/rest/api/storage/uploadPart?file=%s",
                     URLEncoder.encode(file, CodeConstants.ENCODING_UFT_8));
             String resultData = doPostQuery(context, query, data, "body");
             log.debug("resultData = {}", resultData);
@@ -56,7 +56,7 @@ public class FileReader extends RestQuery {
     public boolean createAttachment(RestQueryContext context, String docId, String fileName, String filePath) {
         try {
 
-            String query = String.format("rest/api/storage/document/%s/attachment/create", docId);
+            String query = String.format("/rest/api/storage/document/%s/attachment/create", docId);
             String data = String.format("docID=%s&path=ase:attachmentContainer&fileName=%s&filePath=%s&",
                     docId, URLEncoder.encode(fileName, CodeConstants.ENCODING_UFT_8),
                     URLEncoder.encode(filePath, CodeConstants.ENCODING_UFT_8));
